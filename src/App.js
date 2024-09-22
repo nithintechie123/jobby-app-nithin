21cde4e09504adff4288a2d2bf489e53b5accf3d
root <nithingorintala212121@zohomail.ingit config user.name nithintechie123>
2024-09-22 03:00:25 +0000

    added files

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => (
       <ProtectedRoute exact path="/" component={Home} />
       <ProtectedRoute exact path="/jobs" component={Jobs} />
       <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-      <Route path="/not-found" component={NotFound} />
+      <ProtectedRoute path="/not-found" component={NotFound} />
       <Redirect to="/not-found" />
     </Switch>
   </>
